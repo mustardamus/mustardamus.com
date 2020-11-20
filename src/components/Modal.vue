c8a0920d13e6b072a98a3709d3881de3a1913de6
Sebastian Senf <contact@mustardamus.com>
2020-11-20 13:32:16 +0100

    simplified modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,40 +1,27 @@
 <template>
-  <div v-show="isActive" class="fixed z-10 inset-0 overflow-y-auto">
-    <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-      <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-40 text-center sm:block sm:p-0"
-      >
-        <div
-          class="absolute inset-0 bg-black opacity-90 cursor-pointer"
-          @click="close"
-        ></div>
+  <div
+    v-show="isActive"
+    class="fixed z-8 inset-0 overflow-y-auto cursor-pointer"
+    @click="close"
+  >
+    <div
+      class="flex items-start h-screen min-h-screen justify-center text-center sm:p-5"
+    >
+      <img
+        :src="thumbPath"
+        class="z-10 w-full max-w-screen-lg thumb"
+        v-show="isLoading"
+      />
 
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span
-          class="hidden sm:inline-block sm:align-middle sm:h-screen"
-          aria-hidden="true"
-        >
-          &#8203;
-        </span>
-
-        <div
-          class="inline-block overflow-hidden transform transition-all cursor-pointer lg:max-w-screen-lg sm:my-8 sm:align-middle sm:max-w-screen-sm sm:w-full shadow-2xl"
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby="modal-headline"
-          @click="close"
-        >
-          <img :src="thumbPath" class="w-full thumb" v-show="isLoading" />
-
-          <img
-            :src="distPath"
-            class="w-full"
-            ref="distImg"
-            v-show="!isLoading"
-          />
-        </div>
-      </div>
+      <img
+        ref="distImg"
+        :src="distPath"
+        class="z-10 w-full max-w-screen-lg rounded-lg shadow-2xl"
+        v-show="!isLoading"
+      />
     </div>
+
+    <div class="absolute z-9 inset-0 bg-black opacity-75"></div>
   </div>
 </template>
 
