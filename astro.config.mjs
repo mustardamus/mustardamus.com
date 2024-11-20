827c8a6079406bec5d49cbcf953ed4e09f3923b3
Sebastian Senf <GIT_EMAIL>
2024-11-20 19:46:35 +0100

    add tailwind with unstyled flash fix, add main layout, use layout in pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+
+process.env.ASTRO_TELEMETRY_DISABLED = "1"
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false, // `assets/css/main.css` is imported in the layout, handles "unstyled flash"
+    }),
+  ],
+})
