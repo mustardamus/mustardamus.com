12518205de73269ea047634594eb410c1209bd5f
Sebastian Senf <GIT_EMAIL>
2024-11-23 14:00:26 +0100

    navbar styling and logic, added icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
 
 process.env.ASTRO_TELEMETRY_DISABLED = "1"
 
@@ -9,6 +10,17 @@ export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false, // `assets/css/main.css` is imported in the layout, handles "unstyled flash"
+    }),
+    icon({
+      include: {
+        solar: [
+          "home-bold-duotone",
+          "book-2-bold-duotone",
+          "hamburger-menu-line-duotone",
+          "sun-2-line-duotone",
+          "moon-bold-duotone",
+        ],
+      },
     }),
   ],
 })
