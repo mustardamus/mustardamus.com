f99d497e89e99adfea084e3fac8655997af96307
mustardamus <contact@mustardamus.com>
2025-02-12 12:44:16 +0100

    explicit import of types, dynamic photos album page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,19 @@
+import type { ImageMetadata } from "astro";
+
 interface Link {
   title: string;
   href: string;
   icon: string;
+}
+
+interface Photo {
+  path: string;
+  name: string;
+  import: () => Promise<{ default: ImageMetadata }>;
+}
+
+interface Album {
+  name: string;
+  subName: string | undefined;
+  photos: Photo[];
 }
